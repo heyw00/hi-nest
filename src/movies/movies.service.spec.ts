4ad759ef8f6c24a229ed1070c4ef9ebb17d3ed0e
heyw00 <heywoo12@gmail.com>
2024-01-18 19:20:01 +0900

    lecture 3.1 first unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
-describe('MoviesService', () => {
+describe('MoviesService', () => { 
   let service: MoviesService;
 
   beforeEach(async () => {
@@ -15,4 +15,8 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be 4', () => {
+    expect(2 + 3).toEqual(5)
+  })
 });
