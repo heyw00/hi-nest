7ce6328f40ab6c9ac2e1e09107f60e2b8fd29703
heyw00 <heywoo12@gmail.com>
2024-01-18 18:43:31 +0900

    lecture 2.6 Modules and Dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
-
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 //데코레이터 : 클래스에 함수 기능을 추가할 수 있다. 클래스 위의 함수이고, 클래스를 위해 움직인다.
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
